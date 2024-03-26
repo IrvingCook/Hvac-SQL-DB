acb2ee7977ecd539794e3ed247775e4a42899975
IrvingCook <155038198+IrvingCook@users.noreply.github.com>
2024-03-26 13:16:00 -0400

    Delete Query.sql

--- Query.sql
+++
@@ -1,5 +0,0 @@
--- Irving Query 1: This Query Displays ALL DOCTORS IN A CERTAIN DEPARTMENT, good for appointments, medical capacity, and availability, Referrals
-SELECT doctorID, name 
-FROM Doctors
-WHERE departmentID = 11;
-
