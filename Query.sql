72d09bbafeb6265741bbd03a73a33114c1e25931
IrvingCook <155038198+IrvingCook@users.noreply.github.com>
2024-03-26 12:43:04 -0400

    Create Query.sql

---
+++ Query.sql
@@ -0,0 +1,5 @@
+-- Irving Query 1: This Query Displays ALL DOCTORS IN A CERTAIN DEPARTMENT, good for appointments, medical capacity, and availability, Referrals
+SELECT doctorID, name 
+FROM Doctors
+WHERE departmentID = 11;
+
